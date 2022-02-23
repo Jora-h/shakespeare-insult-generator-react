ee73066b1bd44af3d395ecc780bcd43c283bb967
Hajar Hamza <hajar.hamza1@gmail.com>
2022-02-23 12:32:34 +0100

    add components and generate random sentence from shakespear insults

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Insult from './components/Insult';
+
 
 function App() {
+  const title = "Radom insult form Shakespear";
+  let shakespearinsults=[
+    { insult:"Were such things here as we do speak about? Or have we eaten on the insane root That takes the reason prisoner?", play:"Macbeth"},
+    { insult:"Never hung poison on a tread toad", play:"Richard III"},
+    { insult:"He thinks too much: such men are dangerous. ", play:"Julius Ceasar"},
+    { insult:"Thou calledst me a dog before thou hadst a cause. But since I am a dog, beware my fangs.", play:"The Merchant of Venice"},
+    { insult:"Give me your hand...I can tell your fortune. You are a fool.", play:"The Two Noble Kinsmen"},
+    { insult:"He smells like a fish, a very ancient and fish-like smell, a kind of not-of-the-newest poor-John. A strange fish!", play:"The Tempest"},
+    { insult:"It is a tale Told by an idiot, full of sound and fury, Signifying nothing.", play:"Macbeth"},
+    { insult:"Alas, poor heart, that kiss is comfortless As frozen water to a starved snake.", play:"Titus Andronicus"},
+    { insult:"He hath eaten me out of house and home; he hath put all substance into that fat belly of his.", play:"Henry IV, Part 2"},
+    { insult:"Out, you green-sickness carrion! Out, you baggage! You tallow-face! ", play:"Romeo and Juliet"},
+
+  ]
+  const randomInsult = () => {
+    const index = Math.floor(Math.random() * (shakespearinsults.length - 1));
+    return <Insult insult={shakespearinsults[index].insult} play={shakespearinsults[index].play} />
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <h1>{ title }</h1>
+        <div>{ randomInsult() }</div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/Insult.js
@@ -0,0 +1,8 @@
+function Insult(props){
+
+    const {insult, play}=props;
+    return(
+        <p>{ insult } - { play }</p>
+    )
+}
+export default Insult;
